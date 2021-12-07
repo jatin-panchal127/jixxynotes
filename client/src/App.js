a2b94f12adc1c30dd539bbcecbbcd49546530a66
jatin-panchal127 <jatin.hp482k1@gmail.com>
2021-12-07 21:45:49 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import './App.css';
+import About from "./components/About";
+import Alert from "./components/Alert";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import SignUp from "./components/SignUp";
+import NoteState from "./context/notes/noteState";
+
+
+function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert alert={alert}/>
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home showAlert= {showAlert}/>
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/login">
+                <Login showAlert= {showAlert} />
+              </Route>
+              <Route exact path="/signup">
+                <SignUp showAlert= {showAlert} />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  )
+}
+
+export default App;
